37da8fd705e324acc73947d4b39c57f71ee59dca
Radosław Panuszewski <radoslaw.panuszewski15@gmail.com>
2021-04-18 22:16:53 +0200

    Add builds for Notification Service and Text Message Service

--- local/start-backend.sh
+++ local/start-backend.sh
@@ -1,4 +1,16 @@
+cp -r lost-report-service/src/main/proto/* notification-service/src/main/proto
+cp -r text-message-service/src/main/proto/* notification-service/src/main/proto
+
 cd lost-report-service
 ./gradlew dockerBuild
 cd ..
+
+cd notification-service
+./gradlew dockerBuild
+cd ..
+
+cd text-message-service
+./gradlew dockerBuild
+cd ..
+
 docker-compose -f confluent-platform.yml -f database.yml -f microservices.yml up
