dfcf3d20d9f9eb61f0d6645845854c9f6b72d408
Radosław Panuszewski <radoslaw.panuszewski@webfleet.com>
2021-04-19 19:34:53 +0200

    Fix windows script, remove unnecessary containers from confluent-platform

--- local/start-backend.bat
+++ local/start-backend.bat
@@ -1,1 +1,16 @@
-docker-compose -f confluent-platform.yml -f database.yml -f microservices.yml up
+xcopy /S /Y lost-report-service\src\main\proto\* notification-service\src\main\proto\*
+xcopy /S /Y text-message-service\src\main\proto\* notification-service\src\main\proto\*
+
+cd lost-report-service
+call gradlew dockerBuild
+cd ..
+
+cd notification-service
+call gradlew dockerBuild
+cd ..
+
+cd text-message-service
+call gradlew dockerBuild
+cd ..
+
+call docker-compose -f confluent-platform.yml -f database.yml -f microservices.yml up
